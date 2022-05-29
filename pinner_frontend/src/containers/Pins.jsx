51d563addf6c659a55ec873de2432e350ebedc03
m-zaremba <zaremba_michal@wp.pl>
2022-05-29 12:56:48 +0200

    Pins and Navbar components update

--- pinner_frontend/src/containers/Pins.jsx
+++ pinner_frontend/src/containers/Pins.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
-const Pins = () => {
-  return <div>Pins</div>;
+import PinDetail from '../components/PinDetail';
+import CreatePin from '../components/CreatePin';
+import Feed from '../components/Feed';
+import Navbar from '../components/Navbar';
+import Search from '../components/Search';
+
+
+
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div className="px-2 md px-5">
+      <div className="bg-grey-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetail user={user} />} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
