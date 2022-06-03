abcbe71384bc5e1e5f94c725d47c99cbf144db41
m-zaremba <zaremba_michal@wp.pl>
2022-06-03 06:10:30 +0200

    Add a pin functionality added

--- pinner_frontend/src/components/CreatePin.jsx
+++ pinner_frontend/src/components/CreatePin.jsx
@@ -53,14 +53,44 @@ const CreatePin = ({ user }) => {
     }
   };
 
+  const savePin = () => {
+    if (title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: "pin",
+        title,
+        about,
+        destination,
+        image: {
+          _type: "image",
+          asset: {
+            _type: "reference",
+            _ref: imageAsset?._id,
+          },
+        },
+        userId: user._id,
+        postedBy: {
+          _type: "postedBy",
+          _ref: user._id,
+        },
+        category,
+      };
+      client.create(doc).then(() => {
+        navigate("/");
+      });
+    } else {
+      setFields(true);
+      setTimeout(() => setFields(false), 2000);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
       {fields && (
-        <p className="text-red-500 b-5 text-sl-transition-all duration-150 ease-in">
+        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">
           Please fill all the fields!
         </p>
       )}
-      <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5  w-full">
         <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
           <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
             {loading && <Spinner />}
@@ -103,6 +133,78 @@ const CreatePin = ({ user }) => {
                 </button>
               </div>
             )}
+          </div>
+        </div>
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add your title"
+            className="outline-none text-base sm:text-lg font-bold border-b-2 border-gray-200 p-2"
+          />
+          {user && (
+            <div className="flex gap-2 mt-2 mb-2 items-center bg-white rounded-lg ">
+              <img
+                src={user.image}
+                className="w-10 h-10 rounded-full"
+                alt="user-profile"
+              />
+              <p className="font-bold">{user.userName}</p>
+            </div>
+          )}
+          <input
+            type="text"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="Tell everyone what your Pin is about"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <input
+            type="url"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Add a destination link"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <div className="flex flex-col">
+            <div>
+              <p className="mb-2 font-semibold sm:text-xl">
+                Choose Pin category
+              </p>
+              <select
+                name="choose-pin"
+                id="choosePin"
+                onChange={(event) => setCategory(event.target.value)}
+                className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+              >
+                <option
+                  value="others"
+                  className="sm:text-bg bg-white"
+                  key="others"
+                >
+                  Select Category
+                </option>
+                {categories.map((categoryItem) => (
+                  <option
+                    className="text-base border-0 outline-none capitalize bg-white text-black "
+                    value={categoryItem.name}
+                    key={categoryItem.name}
+                  >
+                    {categoryItem.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-end items-end mt-5">
+              <button
+                type="button"
+                onClick={savePin}
+                className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-dash-none"
+              >
+                Save Pin
+              </button>
+            </div>
           </div>
         </div>
       </div>
